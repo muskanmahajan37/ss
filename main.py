c856940c102d4652914b86d17481838f0e0bc8a0
Fengda HUANG <h@phodal.com>
2016-08-25 12:50:43 +0800

    resize image

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ os.system("screencapture screen.png")
 
 win = gtk.Window()
 
-image = gtk.Image()
-image.set_from_file("screen.png")
-image.show()
+im = gtk.Image()
+pixbuf = gtk.gdk.pixbuf_new_from_file("screen.png")
+scaled_buf = pixbuf.scale_simple(1440,900,gtk.gdk.INTERP_BILINEAR)
+im.set_from_pixbuf(scaled_buf)
+im.show()
 
 vbox = gtk.VBox()
-vbox.pack_start (image)
+vbox.pack_start (im)
 win.add(vbox)
 
 win.fullscreen()
 win.show_all()
+
+win.set_keep_above(True);
 gtk.main()
